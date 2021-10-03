869fe81117d2055ed869da5d275ab08b4215a497
VarnikaB <55554547+VarnikaB@users.noreply.github.com>
2021-10-03 18:48:53 +0530

    Update OracleCon.java

--- src/OracleCon.java
+++ src/OracleCon.java
@@ -9,14 +9,14 @@ class OracleCon
 			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl2","scott","tiger");
 			Statement stmt = con.createStatement();
 			
-		     /*createtable ct = new createtable(); 
-				ct.main(args);
-				System.out.println("Table created successfully");
-				insertinto ins  = new insertinto();
-				ins.main(args);*/
-				System.out.println("Values inserted successfully");
-				query q = new query();
-				q.main(args);
+		     	createtable ct = new createtable(); 
+			ct.main(args);
+			System.out.println("Table created successfully");
+			insertinto ins  = new insertinto();
+			ins.main(args);
+			System.out.println("Values inserted successfully");
+			query q = new query();
+			q.main(args);
 			con.close(); 		      
 		}
 		catch(Exception e)
